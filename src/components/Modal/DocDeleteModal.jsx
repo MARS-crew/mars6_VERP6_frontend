5a66c68852372c11b579d763b31f80afb341d605
mhyoc <105059385+mhyoc@users.noreply.github.com>
2025-01-24 05:08:06 +0900

    feat: 모달에 warning svg 추가

--- src/components/Modal/DocDeleteModal.jsx
+++ src/components/Modal/DocDeleteModal.jsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import DeleteButton from '../Button/DeleteButton';
+import warningIcon from '../../assets/warning-icon.svg';
 
 const DocDeleteModal = ({ onDelete, onCancel }) => {
   const handleOverlayClick = useCallback((e) => {
@@ -14,22 +15,27 @@ const DocDeleteModal = ({ onDelete, onCancel }) => {
       onClick = {handleOverlayClick}
     >
       <div 
-        className = "bg-white w-[500px] h-[250px] rounded-[10px] flex flex-col items-center justify-center">
+        className = "bg-white w-[500px] h-[250px] rounded-[10px] flex flex-col items-center pt-[19px]">
+        <img 
+          src = {warningIcon} 
+          alt = "warning" 
+          className = "w-[38px] h-[33px] mb-4"
+        />
         <div className = "mb-2 space-y-2">
           <div className = "space-y-2">
-            <p id = "modal-title" className = "text-[20px] font-normal text-center">
+            <p id = "modal-title" className = "text-[20px] font-normal text-center leading-[25px]">
               서류를 삭제하면 관련 리스트도
             </p>
-            <p className = "text-[20px] font-normal text-center">
+            <p className = "text-[20px] font-normal text-center leading-[25px]">
               모두 삭제되며 복구할 수 없습니다.
             </p>
           </div>
-          <p className = "text-[20px] font-bold text-center">
+          <p className = "text-[20px] font-bold text-center leading-[25px]">
             정말 삭제하시겠습니까?
           </p>
         </div>
 
-        <div className = "flex gap-[122px] mt-8">
+        <div className = "w-full px-[71px] pb-[29px] mt-auto flex justify-between">
           <DeleteButton onClick = {onDelete} variant = "delete">
             Delete
           </DeleteButton>

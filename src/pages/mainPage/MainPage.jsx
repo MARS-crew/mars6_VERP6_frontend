996dada7c2ce614130a1c9345d156516e96b3328
mhyoc <105059385+mhyoc@users.noreply.github.com>
2025-01-24 05:22:30 +0900

    Revert "fix: 모달 warning svg, padding, line height"
    This reverts commit f06eeac9b1fed29d54330494f005112b3f435be4.

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
-import DocDeleteModal from '../../components/Modal/DocDeleteModal';
+import React from "react";
 
 function MainPage() {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleDelete = () => {
-    console.log('Delete clicked');
-    setShowModal(false);
-  };
-
-  const handleCancel = () => {
-    console.log('Cancel clicked');
-    setShowModal(false);
-  };
-
-  return (
-    <div className = "h-screen flex items-center justify-center">
-      <button
-        onClick = {() => setShowModal(true)}
-        className = "px-4 py-2 bg-red-500 text-white rounded"
-      >
-        삭제 모달 열기
-      </button>
-
-      {showModal && (
-        <DocDeleteModal 
-          onDelete = {handleDelete}
-          onCancel = {handleCancel}
-        />
-      )}
-    </div>
-  );
+  return <div className="bg-[#edeefa] w-40 h-10">mainPage입니다.</div>;
 }
 
 export default MainPage;

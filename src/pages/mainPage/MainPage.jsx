d105c8b5285592ee37a67185639268d5feaad540
mhyoc <gycksl14@naver.com>
2025-01-25 04:03:02 +0900

    feat: 로그인 인풋 분리, 서류 리스트 프레임, 서류 리스트 프레임 추가 기능

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header/Header";
 import AddDocument from "../../components/Button/AddDocument";
+import DocumentList from "../../components/list/DocumentList";
 
 function MainPage() {
+  const [documents, setDocuments] = useState([]);
+
+  const handleAddDocument = () => {
+    setDocuments(prev => [...prev, Date.now()]);
+  };
+
   return (
-    <div className="bg-[#F6F6F6] w-full h-screen">
+    <div className="bg-[#F6F6F6] w-full min-h-screen">
       <Header />
-        <div className = "flex justify-center mt-[30px]">
-          <AddDocument />
+      <div className="flex justify-center">
+        <div className="w-[1194px] mt-[30px]">
+          {documents.length > 0 ? (
+            <>
+              <div className="space-y-[30px] mb-[30px]">
+                {documents.map(id => (
+                  <DocumentList key={id} />
+                ))}
+              </div>
+              <AddDocument onClick={handleAddDocument} />
+            </>
+          ) : (
+            <AddDocument onClick={handleAddDocument} />
+          )}
         </div>
+      </div>
     </div>
   );
 }

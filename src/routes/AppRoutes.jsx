f3ef8efa5b9bf51958b5d3f943ab98c719885677
mhyoc <105059385+mhyoc@users.noreply.github.com>
2025-01-24 17:17:12 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPageView from "../pages/mainPage";
 import DetailPageView from "../pages/detailPage";
+import LoginPageView from "../pages/loginPage";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<MainPageView />} />
     <Route path="/detail-page" element={<DetailPageView />} />
+    <Route path = "/login-page" element = {<LoginPageView />} />
   </Routes>
 );
 

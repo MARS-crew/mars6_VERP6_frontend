00623cb0139c3f27404fcc3a9e3c383487ccd1f2
mhyoc <gycksl14@naver.com>
2025-01-26 02:02:55 +0900

    feat: 서류 리스트명 유효성 검사
    1~20자 사이로 입력

--- src/components/Input/DocumentTypeInput.jsx
+++ src/components/Input/DocumentTypeInput.jsx
@@ -10,7 +10,7 @@ const DocumentTypeInput = ({ value, onChange, onBlur, onKeyDown }) => {
         onBlur = {onBlur}
         onKeyDown = {onKeyDown}
         placeholder = "서류 종류를 입력해주세요."
-        className = "w-[333px] h-[50px] text-[24px] focus:outline-none placeholder-[#B2B2B2]"
+        className = "w-[333px] h-[45px] text-[24px] focus:outline-none placeholder-[#B2B2B2]"
         autoFocus
       />
       <div className = "absolute left-[13px] right-0 bottom-0 border-b border-[#D9D9D9]" />

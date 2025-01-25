e72ce32d446d4c3e4aa02deff5bafe20b06fe1d3
mhyoc <gycksl14@naver.com>
2025-01-25 17:05:23 +0900

    feat: 서류 프레임에 더보기 이미지 삽입

--- src/components/list/DocumentList.jsx
+++ src/components/list/DocumentList.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import moreIcon from '../../assets/images/more-icon.png';
 
 const DocumentList = () => {
   return (
-    <div className = "w-[1194px] h-[189px] bg-white rounded-[8px] shadow-[0_0_2px_2px_rgba(0,0,0,0.10)] p-6">
+    <div className = "w-[1194px] h-[189px] bg-white rounded-[8px] shadow-[0_0_2px_2px_rgba(0,0,0,0.10)] p-6 relative">
+      <img 
+        src = {moreIcon} 
+        alt = "more" 
+        className = "absolute top-[32px] right-[6px] cursor-pointer w-[32px] h-[32px]"
+      />
     </div>
   );
 };

0d4ce01ee7e7aae9d8c4d004a4513f2bb2decfb0
47mango <do_hh@naver.com>
2025-01-25 17:01:35 +0900

    test

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Test from "../pages/index";
+import Papers from "../pages/page-paperlist";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Test />} />
+    <Route path="/paper" element={<Papers />} />
   </Routes>
 );
 

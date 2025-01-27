78cc633798f5e666f706fdebee0c61f8aa8c903e
47mango <do_hh@naver.com>
2025-01-27 21:52:03 +0900

    퍼블리싱 수정

--- src/components/list/DocumentRow/ItemRow.jsx
+++ src/components/list/DocumentRow/ItemRow.jsx
@@ -27,7 +27,7 @@ function ItemRow({ item, isLast }) {
       </div>
 
       <div className="flex items-center justify-between text-sm text-gray-500">
-        <div className="w-[533px] bg-[#9CA2B3] rounded-full h-[36px] mb-2 flex relative">
+        <div className="w-[533px] bg-[#9CA2B3] rounded-full h-[36px] mb-2 flex relative mt-[9px]">
           <div
             className="bg-[#14AE5C] h-[36px] rounded-full absolute"
             style={{ 

7098ffee2fd21af936f2f0399a5a48875350ac8b
47mango <do_hh@naver.com>
2025-01-27 22:52:32 +0900

    pull

--- src/components/Input/LoginInput.jsx
+++ src/components/Input/LoginInput.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const LoginInput = ({ label, type = "text", placeholder, value, onChange, name }) => {
     return (
       <div>
@@ -22,28 +21,3 @@ const LoginInput = ({ label, type = "text", placeholder, value, onChange, name }
   };
   
   export default LoginInput;
-=======
-function LoginInput({ label, type = "text", placeholder, value, onChange, name }) {
-  return (
-    <div>
-      <label 
-        htmlFor = {name}
-        className = "block text-[13px] text-[#7C838A] mb-[16px]"
-      >
-        {label}
-      </label>
-      <input
-        id = {name}
-        name = {name}
-        type = {type}
-        value = {value}
-        onChange = {onChange}
-        placeholder = {placeholder}
-        className = "w-[360px] h-[26px] pl-[7px] pb-[6px] border-b border-[#D9D9D9] text-[12px] focus:outline-none placeholder-[#B2B2B2]"
-      />
-    </div>
-  );
-}
-
-export default LoginInput;
->>>>>>> 566ae9ddf46aa62c6fde61e4569573cfb504d5e7

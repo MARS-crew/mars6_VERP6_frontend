fe4c4a1cf5923464c9e47887730c1d287e836927
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-01-27 16:18:37 +0900

    feat:상세 페이지 팀장 뷰 제작

--- src/pages/detailPage/detailPage.jsx
+++ src/pages/detailPage/detailPage.jsx
@@ -4,14 +4,26 @@ import VersionList from "../../components/list/VersionList/VersionList";
 import VersionListHeader from "../../components/list/VersionList/VersionListHeader";
 import RequestListHeader from "../../components/list/RequestList/RequestListHeader";
 import RequestList from "../../components/list/RequestList/RequestList";
+import trashIcon from "../../assets/svg/Trash.svg";
+import editIcon from "../../assets/svg/Edit.svg";
 
 function DetailPage() {
+  const position = "leader";
   return (
     <div className="bg-[#F6F6F6] w-full h-screen">
       <Header />
       <div className="max-w-[1194px] m-auto flex place-content-between">
         <div className="mt-[30px]">
-          <p className="font-bold text-xl mb-[30px]">Todo list 앱 기획서</p>
+          <div className="flex place-content-between mb-[30px]">
+            <p className="font-bold text-xl">Todo list 앱 기획서</p>
+            {position == "leader" ? (
+              <div className="flex">
+                <p className="text-center">+</p>
+                <img className="w-6 ml-[15px]" src={editIcon} />
+                <img className="w-6 ml-[11px]" src={trashIcon} />
+              </div>
+            ) : null}
+          </div>
           <VersionListHeader />
           <VersionList />
         </div>

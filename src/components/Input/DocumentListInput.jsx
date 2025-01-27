2c7ac5e34f9a9f76d3171a4425c6e55bc49d7444
mhyoc <gycksl14@naver.com>
2025-01-28 01:04:06 +0900

    서류 리스트 인풋 조정

--- src/components/Input/DocumentListInput.jsx
+++ src/components/Input/DocumentListInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function DocumentListInput({ value, onChange, onBlur, onKeyDown }) {
   return (
-    <div className = "w-[333px] relative pl-[20px] pt-[19px]">
+    <div className = "w-[333px] relative pt-[19px]">
       <input
         type = "text"
         value = {value}
@@ -10,10 +10,10 @@ function DocumentListInput({ value, onChange, onBlur, onKeyDown }) {
         onBlur = {onBlur}
         onKeyDown = {onKeyDown}
         placeholder = "서류 종류를 입력해주세요."
-        className = "w-[333px] h-[32px] text-[17px] focus:outline-none placeholder-[#B2B2B2]"
+        className = "w-[333px] h-[32px] text-[17px] pl-[6px] focus:outline-none placeholder-[#B2B2B2]"
         autoFocus
       />
-      <div className = "absolute left-[13px] right-0 bottom-0 border-b border-[#D9D9D9]" />
+      <div className = "border-b border-[#D9D9D9]" />
     </div>
   );
 }

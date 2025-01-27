51b8ce274d919a14e5418a8a95c6a1351312c0e4
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-01-27 17:27:54 +0900

    feat:요청 상세 모달

--- src/components/list/RequestList/RequestList.jsx
+++ src/components/list/RequestList/RequestList.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import DownloadIcon from "../../../assets/svg/Download.svg";
 import ArrowDownIcon from "../../../assets/svg/ArrowDown.svg";
 import ArrowUpIcon from "../../../assets/svg/ArrowUp.svg";
+import DescriptionIcon from "../../../assets/svg/Description.svg";
 
 import StateButton from "../../stateButton/StateButton";
 
 function RequestList({ no, title, state, date, writer, open }) {
+  const [modalState, setModalState] = useState(false);
+  const handleModal = () => {
+    setModalState(!modalState);
+  };
   return (
-    <div className="w-[582px] h-[86px] bg-white rounded-lg items-center justify-center pt-[22px] drop-shadow-lg">
+    <div className="w-[582px] bg-white rounded-lg items-center justify-center pt-[22px] drop-shadow-lg">
       <div className="h-6 flex place-content-between items-center ml-[30px] mr-[25px] text-[15px] ">
         <div className="w-[5%] text-[#8E98A8] font-medium">{no}1</div>
         <div className="text-center">{title}asdkjsakjas.ppt</div>
@@ -16,10 +21,28 @@ function RequestList({ no, title, state, date, writer, open }) {
         <div className="text-center">{date}2025.01.01</div>
         <img src={DownloadIcon} />
       </div>
-      {open == "open" ? (
-        <img className="m-auto mt-[17px]" src={ArrowUpIcon} />
+      {modalState ? (
+        <div className="mx-[21px] mt-[21px]">
+          <div className="flex mb-[9px]">
+            <img src={DescriptionIcon} />
+            <div className="ml-2 font-normal text-[15px]">작업 내역</div>
+          </div>
+          <div className="w-[527px] h-[151px] border border-[#8E98A8] rounded-lg p-4">
+            마이페이지 화면 작업 내용 - Description 작성 완료 - 오탈자 수정 완료
+            관리 페이지 작업 내용 - 오탈자 및 이미지 변경 - ui 변경
+          </div>
+          <img
+            onClick={handleModal}
+            className="m-auto mt-[13px]"
+            src={ArrowUpIcon}
+          />
+        </div>
       ) : (
-        <img className="m-auto mt-[17px]" src={ArrowDownIcon} />
+        <img
+          onClick={handleModal}
+          className="m-auto mt-[17px]"
+          src={ArrowDownIcon}
+        />
       )}
     </div>
   );

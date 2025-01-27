d9406505b9b4ca32a5293ede989d06dee9f4ff9c
mhyoc <gycksl14@naver.com>
2025-01-27 15:58:13 +0900

    feat: 로그인 페이지 추가 퍼블리싱
    컨테이너, 배경컬러

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -22,40 +22,42 @@ const LoginPage = () => {
   };
 
   return (
-    <div className = "h-screen flex items-center justify-center bg-white">
-      <div className = "w-[360px]">
-        <h2 className = "text-[30px] font-bold text-center mb-[50px]">
-          로그인
-        </h2>
-        <form className = "space-y-[25px]" onSubmit = {handleLogin}>
-          <LoginInput
-            label = "아이디"
-            name = "id"
-            placeholder = "아이디를 입력해주세요"
-            value = {loginForm.id}
-            onChange = {handleInputChange}
-          />
-          <LoginInput
-            label = "비밀번호"
-            name = "password"
-            type = "password"
-            placeholder = "비밀번호를 입력해주세요"
-            value = {loginForm.password}
-            onChange = {handleInputChange}
-          />
+    <div className = "h-screen flex items-center justify-center bg-[#F6F6F6]">
+      <div className = "w-[530px] h-[646px] bg-white rounded-[24px] shadow-[0_0_2px_2px_rgba(0,0,0,0.10)] flex items-center justify-center">
+        <div className = "w-[360px]">
+          <h2 className = "text-[30px] font-bold text-center mb-[50px]">
+            로그인
+          </h2>
+          <form className = "space-y-[25px]" onSubmit = {handleLogin}>
+            <LoginInput
+              label = "아이디"
+              name = "id"
+              placeholder = "아이디를 입력해주세요"
+              value = {loginForm.id}
+              onChange = {handleInputChange}
+            />
+            <LoginInput
+              label = "비밀번호"
+              name = "password"
+              type = "password"
+              placeholder = "비밀번호를 입력해주세요"
+              value = {loginForm.password}
+              onChange = {handleInputChange}
+            />
 
-          <div className = "mt-[25px] font-semibold">
-            <LoginButton type = "submit">
-              로그인
-            </LoginButton>
-          </div>
-          
-          <div className = "text-center mt-[25px] font-medium">
-            <a href = "#" className = "text-[15px] text-gray-900">
-              회원가입
-            </a>
-          </div>
-        </form>
+            <div className = "mt-[25px] font-semibold">
+              <LoginButton type = "submit">
+                로그인
+              </LoginButton>
+            </div>
+            
+            <div className = "text-center mt-[25px] font-medium">
+              <a href = "#" className = "text-[15px] text-gray-900">
+                회원가입
+              </a>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );

e2ef6dfa9d130f6c0c0446537d91d53668b82384
mhyoc <gycksl14@naver.com>
2025-02-06 01:50:39 +0900

    refactor: 토큰 관리 방식을 localStorage에서 Recoil로 변경

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          // target: env.VITE_API_URL,
-          target: 'http://mars-crew.shop:26080',
+          target: env.VITE_API_URL,
           changeOrigin: true,
           secure: false,
           ws: true,

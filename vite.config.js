a7d7575197c7a269258e461240290eeb699d02bc
mhyoc <gycksl14@naver.com>
2025-02-06 03:57:40 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: "./postcss.config.cjs",
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://mars-crew.shop:26080',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        credentials: 'include'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    css: {
+      postcss: "./postcss.config.cjs",
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          credentials: 'include'
+        }
       }
-    }
-  },
+    },
+  }
 });

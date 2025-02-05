fe6f1b48e3f4b2e5aa6589be4668c0a225971d79
47mango <do_hh@naver.com>
2025-02-05 11:49:42 +0900

    test

--- src/api/client.jsx
+++ src/api/client.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000", //예시
+  baseURL: process.env.REACT_APP_API_URL || "http://mars-crew.shop:26080/api", //예시
   headers: {
     "Content-Type": "application/json",
   },
